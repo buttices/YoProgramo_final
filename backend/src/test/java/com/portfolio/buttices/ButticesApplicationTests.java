09371a48388d6ac6ee90a5bebb38abe716a117c6
buttices <99561479+buttices@users.noreply.github.com>
2022-09-16 01:08:38 -0300

    Sumé springboot

---
+++ backend/src/test/java/com/portfolio/buttices/ButticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.buttices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
